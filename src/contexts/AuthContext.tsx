c5330db81de73f41d39f7ee82d5ab46a5f281a2b
Willian Komada <willian10komada@hotmail.com>
2022-01-10 20:11:51 -0300

    first commit

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,126 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
+import Router from "next/router";
+import { api } from "../services/apiClient";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+  signOut: () => void;
+  user: User;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  children: ReactNode; // Tipagem no qual o children pode receber qualquer coisa (Componente, textos, números)
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+let authChannel: BroadcastChannel;
+
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+
+  authChannel.postMessage("signOut");
+
+  Router.push("/");
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel("auth");
+
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case "signOut":
+          signOut();
+          break;
+        default:
+          break;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
+
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      // sessionStorage = As infos ficam disponíveis apenas durante a sessão
+      // Example: Fechou o navegador, perdeu a sessão, perdeu as infos
+      // localStorage =  Next não é apenas browser, então utilizar essa opção precisaria de muita adaptação
+
+      // cookies = Acesso em ambos os locais (Client = Browser / Servidor = Node (Next))
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn,
+        signOut,
+        user,
+        isAuthenticated,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
